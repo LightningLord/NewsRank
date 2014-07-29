e772c93c74004bfee3c8093d15bcd76f918cb4aa
Jason Lo <mr.lo.mhs@gmail.com>
2014-07-28 22:43:51 -0700

    an example test for subscriptions#destroy

--- app/controllers/subscriptions_controllers.rb
+++ app/controllers/subscriptions_controllers.rb
@@ -5,4 +5,13 @@ class SubscriptionsController < ApplicationController
     Subscription.destroy(feed: feed, user: current_user)
   end
 
+  #you can test this method with something like this
+
+  # describe '#destroy' do
+  #   #factory girl a feed
+  #   it 'destroys a subscription or whatever' do
+  #     expect{delete :destroy, :id => feed.id}.to change{Subscription.count}.by(-1)
+  #   end
+  # end
+
 end

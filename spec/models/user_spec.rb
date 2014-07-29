0aa1e9b7fd68a437745a76b71d285731a0ce9b7a
Jason Lo <mr.lo.mhs@gmail.com>
2014-07-28 22:27:05 -0700

    implement user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,15 @@ describe User do
   describe "validations" do
     it {should validate_uniqueness_of :email}
     it {should have_many :feeds}
+    it {should have_many :subscriptions}
   end
+
+  #probably want something like this
+  it 'articles captures all articles of all feeds'
+    #psuedocode: create some articles with feeds using let blocks
+    #create a user too. associate the articles and feeds and users properly
+    #expect that articles returns articles.to_json
+    #also there could be an issue with Article#articles having a local variable articles
 
 
 

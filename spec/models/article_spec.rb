9a7d373f3bc315c9838c3e58c17bcc8746c0a5ed
Jason Lo <mr.lo.mhs@gmail.com>
2014-07-28 22:27:57 -0700

    implement article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,17 +5,13 @@ describe Article do
   describe "validations" do
     it {should allow_value(1).for(:published)}
     it {should validate_uniqueness_of :canonical_url}
-
-    it "belongs to a feed" do
-    test_feed = Feed.create()
-    article.feed_id = test_feed.id
-    expect(article.feed.id).to eq(test_feed.id)
-    end
+    it {should belong_to :feed}
 
     describe "#add_article" do
       it "adds an article to the database" do
+        #probably want to do something like this:
+        # expect {article.add_article(valid args)}.to change{Article.count}.by(1)
 
-        expect(article.count).to
       end
     end
   end
